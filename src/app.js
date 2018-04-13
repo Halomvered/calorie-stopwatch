014f219ff17377139147ca7559aa758d5c94fb33
Halom Vered <halom.vered@gmail.com>
2018-04-13 16:55:25 +0300

    add normalize.css + make sure all css files are imported into app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/styles.scss';
 import AppRouter from './routers/AppRouter'
+import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
+import 'normalize.css/normalize.css';
 
 
 ReactDOM.render(<AppRouter />, document.querySelector('#react-app'))
